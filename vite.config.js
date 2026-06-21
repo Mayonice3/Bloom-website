866c318ce3503d6fb6ec51595cedf91517d8f53d
Mayonice3 <123068209+Mayonice3@users.noreply.github.com>
2026-06-21 15:59:06 +0530

    allowed v0 for vercel editing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Bloom-website/",
+  server: {
+    allowedHosts: [
+      'sb-3gzs3ppon357.vercel.run'
+    ]
+
+  }
 });
